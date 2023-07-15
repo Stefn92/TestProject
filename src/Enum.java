4b143d3dc3b1b3aa518d3402694c5e426b8fc1a3
Stefn92 <steffen.schneider02@gmail.com>
2023-07-15 17:58:21 +0200

    Test

--- src/Enum.java
+++ src/Enum.java
@@ -3,6 +3,7 @@ public class Enum {
     public static void main(String[] args) {
         WeekDay day = WeekDay.FRIDAY;
         System.out.println(day);
+        System.out.println("Test");
     }
 
     public enum WeekDay {
